a353485fcb028485bc6814cf72c0771bafa5c29a
brunoqp78 <bruno.queiroz@iftm.edu.br>
2019-10-02 15:26:47 -0300

    comentarios aula

---
+++ 3-JS-Fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// comentário de uma linha
+console.log('uma linha')
+
+/* comentário de várias linhas
+outra linha
+outra linha
+*/
+console.log('várias linhas')
+
+/* outro modo
+* outro modo
+* outro modo
+*/
+
+console.log('outro modo de várias linhas')
