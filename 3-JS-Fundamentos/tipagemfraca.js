98e868bef20c57332275070a3b8719c3c0db2817
brunoqp78 <bruno.queiroz@iftm.edu.br>
2019-10-02 15:53:04 -0300

    fracamente tipada

---
+++ 3-JS-Fundamentos/tipagemfraca.js
@@ -0,0 +1,18 @@
+/*
+    JavaScript é fracamente tipada
+    JavaScript tem tipagem dinamica
+    Dificulta a encontrar erro
+    Facilita o desenvolvimento
+*/
+let q = 'legal'
+console.log(q)
+// typeof retorna o tipo de dados de q
+console.log(typeof q)
+
+q = 10
+console.log(q)
+console.log(typeof q)
+
+q = 11.111
+console.log(q)
+console.log(typeof q)
