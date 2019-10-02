ae1ca93bf72167b46d7eec3125b53009e3130654
brunoqp78 <bruno.queiroz@iftm.edu.br>
2019-10-02 15:32:10 -0300

    aula de variaveis

---
+++ 3-JS-Fundamentos/variaveis e constantes.js
@@ -0,0 +1,21 @@
+// forma de declarar variáveis.
+// definir um nome que reflita o conteúdo da variável.
+var telefone = '(34) 34241121'
+
+// variável normal
+var a=3
+
+// outra forma de declarar variáveis, mais moderna e atual. Usar o let
+let b = 4
+
+// o let não permite redeclarar a variável. var pode fazer, o problema dela
+var a = 4
+// let b = 5 - não pode redeclarar a let
+console.log(a,b)
+
+
+// declarar constantes - const
+
+const c = 5
+//c = 30 - não pode modificar a constante
+console.log(c)
