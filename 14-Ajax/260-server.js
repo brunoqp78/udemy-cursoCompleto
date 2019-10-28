ad92aa727beaa65e285af74aa82a42fea7aa429d
brunoqp78 <bruno.queiroz@iftm.edu.br>
2019-10-28 19:23:18 -0300

    fim aula ajax mais arquivos gulp

---
+++ 14-Ajax/260-server.js
@@ -0,0 +1,66 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const multer = require('multer') // manipular arquivo
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+//definimos a rota
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
+//servir os arquivos estaticos
+app.use(express.static('.'))
+//qualquer requisição
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//app.get('/teste', (req, res)=>res.send('Ok'))
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar/', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+
+})
+
+
+// app.get('/parOuImpar/:numero', (req, res) => {
+//     // req.body
+//     // req.query
+//     // req.params
+//     const par = parseInt(req.query.numero) % 2 === 0
+//     res.send({
+//         resultado: par ? 'par' : 'impar'
+//     })
+// })
+
+app.listen(8081, () => console.log('Executando...'))
+
+
